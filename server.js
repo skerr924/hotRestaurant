d0a2734f9fa672e4d5ffe34f515065b5e47df6bf
Sarah Kerr <sarah.kerr93@gmail.com>
2020-08-03 19:16:11 -0400

    added routes to start, based on StarWars application

--- server.js
+++ server.js
@@ -22,3 +22,72 @@ var reservations = [
     id: "sarahkerr"
   }
 ];
+
+var waitingList = []; 
+
+
+// Routes
+// =============================================================
+
+
+//displays homepage 
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+//displays tables page 
+app.get("/tables", function(req, res) {
+  res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+//displays reserve page 
+app.get("/reserve", function(req, res) {
+  res.sendFile(path.join (__dirname, "reserve.html")); 
+});
+
+
+// Pulling the list of reservations from the database 
+app.get("api/reservations", function(req, res) {   //come back to this! 
+  var data = req.params;
+  console.log(data);
+
+  for (var i = 0; i < data.length; i++) {
+      return res.json(data.name);
+  }
+
+  return res.json(false);
+});
+
+
+// Create new reservation - takes in JSON input
+app.post("/api/reservations", function(req, res) { //how to link this to front-end form 
+  var newReservation = req.name;
+  console.log(newReservation);
+  reservations.push(newReservation);
+  res.json(newReservation);
+
+
+});
+
+//Pulling waitlist from the database 
+app.get("api/waitlist", function(req, res) {   //come back to this! 
+  var data = req.params;
+  console.log(data);
+
+  for (var i = 0; i < data.length; i++) {
+      return res.json(data.name);
+  }
+
+  return res.json(false);
+});
+
+
+// Create new waitlist member 
+app.post("/api/waitlist", function(req, res) { //how to link this to front-end form 
+  var newWaitList = req.name;
+  console.log(newWaitList);
+  reservations.push(newWaitList);
+  res.json(newWaitList);
+
+
+});
